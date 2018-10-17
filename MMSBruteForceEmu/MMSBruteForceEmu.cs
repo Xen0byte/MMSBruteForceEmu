b93f2c256ce093dea4539c23dc5c6c048dc0788c
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2018-10-17 13:15:59 +0100

    Update Solution

--- MMSBruteForceEmu/MMSBruteForceEmu.cs
+++ MMSBruteForceEmu/MMSBruteForceEmu.cs
@@ -48,8 +48,7 @@ namespace MMSBruteForceEmu
         public static string RandomString(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length)
-              .Select(s => s[Random.Next(s.Length)]).ToArray());
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[Random.Next(s.Length)]).ToArray());
         }
 
         public static void RunAttack(IWebDriver driver)
